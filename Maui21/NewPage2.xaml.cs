2bbbb068e39a6f615af92f890d765a82b1a78ef2
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2026-03-09 14:13:14 +0100

    Zadanie 21.4 skonczone i wszystkie zadania skonczone

--- Maui21/NewPage2.xaml.cs
+++ Maui21/NewPage2.xaml.cs
@@ -6,4 +6,38 @@ public partial class NewPage2 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	void OnPickerSelected(object sender, EventArgs e)
+	{
+		if (PickerTemat.SelectedIndex == 0) 
+		{ 
+			PickerPriorytet.IsEnabled = true;
+			PickerPriorytet.ItemsSource = new[] {"Niski","Œredni","Wysoki" };
+
+        }
+		else if (PickerTemat.SelectedIndex == 1)
+		{	
+			PickerPriorytet.IsEnabled = true;
+            PickerPriorytet.ItemsSource = new[] { "Niski", "Œredni" };
+        }
+		else
+		{
+			PickerPriorytet.IsEnabled = false;
+			PickerPriorytet.SelectedIndex = -1;
+        }
+    }
+
+	void OnButtonClicked(object sender, EventArgs e)
+	{
+		var imie = ImieEntry.Text;
+		var email = EmailEntry.Text;
+		var temat = PickerTemat.SelectedItem;
+		var priorytet = PickerPriorytet.SelectedItem;
+		if (priorytet == null) priorytet = "Brak";
+        var kopia = Kopia.IsChecked;
+		if(!string.IsNullOrWhiteSpace(imie) && !string.IsNullOrWhiteSpace(email) && temat != null)
+		{
+			DisplayAlert("Dziêkujemy!", $"Twoja wiadomoœæ zosta³a wys³ana.\nImiê: {imie}\nEmail: {email}\nTemat: {temat}\nPriorytet: {priorytet}\nKopia: {(kopia ? "Tak" : "Nie")}", "OK");
+        }
+	}
 }
